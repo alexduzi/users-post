95317080bfb5d8bde5c27d7015f7f334d6db27ef
Alex Duzi <alex.henrique.duzi>
2024-10-08 19:38:46 -0300

    user repository e integração com mongoDB

--- src/main/java/com/alexduzi/workshopuserspost/resources/UserResource.java
+++ src/main/java/com/alexduzi/workshopuserspost/resources/UserResource.java
@@ -1,29 +1,28 @@
 package com.alexduzi.workshopuserspost.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alexduzi.workshopuserspost.domain.User;
+import com.alexduzi.workshopuserspost.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1", "Maria Silva", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
 		
-		List<User> list = new ArrayList<>();
+		List<User> users = userService.findAll();
 		
-		list.addAll(Arrays.asList(maria, alex));
-		
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(users);
 	}
 }
---
+++ src/main/java/com/alexduzi/workshopuserspost/services/UserService.java
@@ -0,0 +1,20 @@
+package com.alexduzi.workshopuserspost.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexduzi.workshopuserspost.domain.User;
+import com.alexduzi.workshopuserspost.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+}
