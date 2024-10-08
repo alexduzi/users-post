f2f2f37f41dc04a99186d7cdcca598ec8b746a4b
Alex Duzi <alex.henrique.duzi>
2024-10-07 21:38:00 -0300

    primeiro commit

---
+++ src/main/java/com/alexduzi/workshopuserspost/WorkshopuserspostApplication.java
@@ -0,0 +1,13 @@
+package com.alexduzi.workshopuserspost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopuserspostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopuserspostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexduzi/workshopuserspost/WorkshopuserspostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexduzi.workshopuserspost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopuserspostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
