6a454bf8a7c0b42796234c4857d03e7ef380179d
Alex Duzi <alex.henrique.duzi>
2024-10-08 21:37:28 -0300

    entity post com user aninhado

--- src/main/java/com/alexduzi/workshopuserspost/config/Instantiation.java
+++ src/main/java/com/alexduzi/workshopuserspost/config/Instantiation.java
@@ -1,12 +1,15 @@
 package com.alexduzi.workshopuserspost.config;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.alexduzi.workshopuserspost.domain.Post;
 import com.alexduzi.workshopuserspost.domain.User;
+import com.alexduzi.workshopuserspost.repository.PostRepository;
 import com.alexduzi.workshopuserspost.repository.UserRepository;
 
 @Configuration
@@ -14,17 +17,25 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, LocalDate.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, LocalDate.now(), "Bom dia", "Acordei feliz hoje!", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
