c0735a96bc5f0f78d373ae6b41ba353e98e99faa
Alex Duzi <alex.henrique.duzi>
2024-10-08 22:35:31 -0300

    consulta com @Query

--- src/main/java/com/alexduzi/workshopuserspost/repository/PostRepository.java
+++ src/main/java/com/alexduzi/workshopuserspost/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.alexduzi.workshopuserspost.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alexduzi.workshopuserspost.domain.Post;
@@ -10,5 +11,9 @@ import com.alexduzi.workshopuserspost.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	public List<Post> findByTitleContainingIgnoreCase(String text);
+	// consulta por query method
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
