af63b8d068606b834587168865b42ca42e8f95ac
Alex Duzi <alex.henrique.duzi>
2024-10-08 22:07:20 -0300

    adicionando comentários aos posts

--- src/main/java/com/alexduzi/workshopuserspost/config/Instantiation.java
+++ src/main/java/com/alexduzi/workshopuserspost/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.alexduzi.workshopuserspost.config;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.alexduzi.workshopuserspost.domain.Post;
 import com.alexduzi.workshopuserspost.domain.User;
 import com.alexduzi.workshopuserspost.dto.AuthorDTO;
+import com.alexduzi.workshopuserspost.dto.CommentDTO;
 import com.alexduzi.workshopuserspost.repository.PostRepository;
 import com.alexduzi.workshopuserspost.repository.UserRepository;
 
@@ -37,6 +39,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, LocalDate.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, LocalDate.now(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", LocalDate.now(), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", LocalDate.now(), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", LocalDate.now(), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		
+		post2.getComments().add(c3);
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
